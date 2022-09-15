dcbd57db7e9e0219e7112f72f03796e022d16839
DLLM <DLLM@gft.com>
2022-09-14 23:27:42 -0300

    Subindo configuração do jwt

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -6,9 +6,27 @@ import { LocalStrategy } from './local.strategy';
 import { UserService } from '../user/user.service';
 import { PrismaService } from '../database/prisma.service';
 import { MailModule } from '../mail/mail.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [UserModule, PassportModule, MailModule],
-  providers: [AuthService, LocalStrategy, PrismaService, UserService],
+  imports: [
+    UserModule,
+    PassportModule,
+    MailModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    PrismaService,
+    UserService,
+    JwtStrategy,
+  ],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.userService.getByEmail(username);
@@ -14,5 +18,12 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
